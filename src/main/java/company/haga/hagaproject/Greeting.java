615e3e1f0fcd486bbacd9b175ff12bd305995a3e
nengkya <nengkya@yahoo.com>
2023-06-04 08:05:57 +0700

    import company.haga.hagaproject.Greeting;

---
+++ src/main/java/company/haga/hagaproject/Greeting.java
@@ -0,0 +1,5 @@
+package company.haga.hagaproject;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ src/main/java/company/haga/hagaproject/GreetingController.java
@@ -0,0 +1,30 @@
+package company.haga.hagaproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import company.haga.hagaproject.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s";
+	private final AtomicLong counter = new AtomicLong();
+	int i;
+
+	@GetMapping("greeting")
+	public Greeting greeting(
+		@RequestParam(value = "name", defaultValue = "Haga")
+		String name) {
+
+		//return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		i = i + 1;
+		return new Greeting(i, String.format(template, name));
+
+
+	}
+
+}
