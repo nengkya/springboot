4ff599d24faf1c4660cf45672528f7b6e2830e58
nengkya <nengkya@yahoo.com>
2023-06-04 23:06:35 +0700

    Integer integer = Integer.valueOf(0);

---
+++ src/main/java/company/haga/hagaproject/IntegerWithMethod.java
@@ -0,0 +1,16 @@
+public class IntegerWithJava {
+
+
+	public static void main(String[] arguments) {
+
+		Integer integer = Integer.valueOf(0);
+
+		integer = integer + 1;
+
+		integer++;
+
+		System.out.println(integer);
+
+	}
+
+}
