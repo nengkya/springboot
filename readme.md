37c000839fb9f3e449155b58521cfa6ff847bbb7
nengkya <nengkya@yahoo.com>
2023-06-07 21:43:13 +0700

    start class Person

--- readme.md
+++ readme.md
@@ -1,13 +1,3 @@
-[modifier](https://www.w3schools.com/java/java_modifiers.asp)
-
-[extends](https://www.w3schools.com/java/ref_keyword_extends.asp)
-
-[implements](https://www.w3schools.com/java/ref_keyword_implements.asp)
-
-https://spring.io/guides
-
-[LocalDate](https://www.javatpoint.com/java-localdate)
-
 [Person.java](https://algs4.cs.princeton.edu/31elementary/Person.java.html)
 
 https://en.wikipedia.org/wiki/Jackson_(API)
--- src/main/java/company/haga/hagaproject/JavaTimeFormatDateTimeFormatter.java
+++ src/main/java/company/haga/hagaproject/JavaTimeFormatDateTimeFormatter.java
@@ -6,8 +6,14 @@ class JavaTimeFormatDateTimeFormatter {
 	public static void main(String[] arguments) {
 
 		LocalDate localDate = LocalDate.now();
+		System.out.println(localDate);		
 
+		/*
+		ISO_OFFSET_DATE 	ISO Date with offset 			'2011-12-03+01:00'
+		ISO_DATE 			ISO Date with or without offset '2011-12-03+01:00'; '2011-12-03'
+		*/
 		String localDateString = localDate.format(DateTimeFormatter.ISO_DATE);
+		System.out.println(localDateString);
 
 	}
 
