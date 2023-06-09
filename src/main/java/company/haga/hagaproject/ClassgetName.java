98821176fe805927187c0e5d0637d05e23700005
nengkya <nengkya@yahoo.com>
2023-06-09 21:50:41 +0700

    update readme.md

---
+++ src/main/java/company/haga/hagaproject/ClassgetName.java
@@ -0,0 +1,14 @@
+package company.haga.hagaproject.myclass.get.name;
+
+//import java.lang.Class;
+
+public class ClassgetName {
+
+	public static void main(String[] arguments) {
+
+		ClassgetName classGetName = new ClassgetName();
+		Class myClass = classGetName.getClass();
+
+	}
+
+}
