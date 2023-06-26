6965d9f33d47145f18f6194cdab0870ac0fecc95
nengkya <nengkya@yahoo.com>
2023-06-27 06:02:37 +0700

    Thread object is executed with start() method. Thread object action using run() method.

--- src/main/java/company/haga/hagaproject/MyThread.java
+++ src/main/java/company/haga/hagaproject/MyThread.java
@@ -1,9 +1,16 @@
 public class MyThread extends Thread {
 
+	/*thread action*/
+	public void run() {
+		System.out.println("Thread object is executed with start() method.");
+		System.out.println("Thread object action using run() method.");
+	}
+
+
 	public static void main(String[] arguments) {
-
-
-
+		MyThread myThread = new MyThread();
+		/*thread execution*/
+		myThread.start();
 	}
 
 }
